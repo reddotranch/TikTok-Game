a4925dbee384448717cf112b161683c4b6f65c31
Ubuntu <ubuntu@ip-172-31-46-62.us-west-2.compute.internal>
2025-06-11 22:54:42 +0000

    update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "gitaction-devops" # Replace with your actual S3 bucket name
+    bucket = "betech-backend" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-west-2"
   }
